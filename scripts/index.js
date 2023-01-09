0623c70efc91285a6c42cdd5181fcc4ab1b7cd80
Anton Glazunov <kampel2012@yandex.ru>
2023-01-09 20:07:04 +0500

    hotfix мелкие баги по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,38 @@
-let btnsLikePath = Array.from(document.querySelectorAll('.card__btn'))
-
-btnsLikePath.forEach(item => {
-  item.addEventListener('click', () => {
-    if (item.getAttribute('active') == null) {
-      item.setAttribute('active', '')
-      item.querySelector('.card__btn-like').setAttribute('src', "./images/icon-like_active.svg")
-    } else {
-      item.removeAttribute('active', '')
-      item.querySelector('.card__btn-like').setAttribute('src', "./images/icon-like.svg")
-    }
-  })
-});
-
 let profile = document.querySelector('.profile')
-let overlay = document.querySelector('.overlay')
-let profileBtnEdit = profile.querySelector('.profile__btn_edit')
-let overlayCloseBtn = document.querySelector('.pop-up__close-icon')
+let popUp = document.querySelector('.pop-up')
+let profileEditBtn = profile.querySelector('.profile__btn_type_edit')
+let overlayCloseBtn = popUp.querySelector('.pop-up__btn_type_close')
+let popUpForm = popUp.querySelector('.pop-up__form')
+let profileName = profile.querySelector('.profile__name')
+let profileJob = profile.querySelector('.profile__subtitle')
 
-profileBtnEdit.addEventListener('click', () => {
-  overlay.classList.add('overlay_opened')
-  overlay.querySelector('.input-name').value = profile.querySelector('.profile__name').outerText
-  overlay.querySelector('.input-job').value = profile.querySelector('.profile__subtitle').outerText
-})
-
-overlayCloseBtn.addEventListener('click', closeOverlay)
-
-function closeOverlay() {
-  overlay.classList.remove('overlay_opened')
+function popUpOpenOverlay() {
+  popUp.classList.add('pop-up_opened')
+  popUp.querySelector('.pop-up__input-name').value = profileName.textContent
+  popUp.querySelector('.pop-up__input-job').value = profileJob.textContent
 }
 
-overlay.querySelector('.pop-up').addEventListener('submit', (e) => {
-  e.preventDefault()
-  let valueName = overlay.querySelector('.input-name').value
-  let valueJob = overlay.querySelector('.input-job').value
-  if ((valueName == '') || (valueJob == '')) {
+profileEditBtn.addEventListener('click', popUpOpenOverlay)
+
+function popUpValueEdit() {
+  let valueInputName = popUp.querySelector('.pop-up__input-name').value
+  let valueInputJob = popUp.querySelector('.pop-up__input-job').value
+  if ((valueInputName == '') || (valueInputJob == '')) {
     alert('Заполните все поля ввода!')
   } else {
-    profile.querySelector('.profile__name').innerHTML = valueName
-    profile.querySelector('.profile__subtitle').innerHTML = valueJob
-    closeOverlay()
+    profileName.textContent = valueInputName
+    profileJob.textContent = valueInputJob
   }
+}
+
+function popUpCloseOverlay() {
+  popUp.classList.remove('pop-up_opened')
+}
+
+overlayCloseBtn.addEventListener('click', popUpCloseOverlay)
+
+popUpForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  popUpValueEdit()
+  popUpCloseOverlay()
 })
-
-
