b7d55ce032d565037f4a631fbf81114ccdeb08a8
Anton Glazunov <kampel2012@yandex.ru>
2023-03-21 12:32:37 +0500

    refactor вынести в конструктор кнопки для производ

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -7,6 +7,12 @@ export class Card {
     this._cardElement = this._createCardElement();
     this._cardImage = this._cardElement.querySelector('.card__image');
     this._cardTitle = this._cardElement.querySelector('.card__title');
+    this._cardButtonDelete = this._cardElement.querySelector(
+      '.card__btn_type_delete',
+    );
+    this._cardButtonLike = this._cardElement.querySelector(
+      '.card__btn_type_like',
+    );
     this._fillCardInfo();
     this._setCardHandling();
   }
@@ -29,26 +35,23 @@ export class Card {
   }
 
   _setCardHandling() {
-    this._cardElement.addEventListener('click', e => {
-      this._removeCardIfRequired(e);
-      this._switchLikeActiveIfRequired(e);
+    this._cardButtonLike.addEventListener('click', () => {
+      this._switchLikeActiveIfRequired();
+    });
+    this._cardButtonDelete.addEventListener('click', () => {
+      this._removeCardIfRequired();
     });
     this._cardImage.addEventListener('click', () =>
       this._handleCardClick({ name: this._name, link: this._link }),
     );
   }
 
-  _removeCardIfRequired(e) {
-    if (e.target.classList.contains('card__btn_type_delete')) {
-      this._cardElement.remove();
-      this._cardElement = null;
-    }
+  _removeCardIfRequired() {
+    this._cardElement.remove();
+    this._cardElement = null;
   }
 
-  _switchLikeActiveIfRequired(e) {
-    const btnLikeTarget = e.target;
-    if (btnLikeTarget.classList.contains('card__btn')) {
-      btnLikeTarget.classList.toggle('card__btn_like_active');
-    }
+  _switchLikeActiveIfRequired() {
+    this._cardButtonLike.classList.toggle('card__btn_like_active');
   }
 }
